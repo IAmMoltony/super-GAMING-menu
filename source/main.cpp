d02161048a1ad3fb720a3b5b9db8fb579bf4d909
IAmMoltony <koronavirusnyj@gmail.com>
2024-10-24 21:02:07 +0500

    add specifying config file via argument

--- source/main.cpp
+++ source/main.cpp
@@ -4,21 +4,24 @@
 #include <iostream>
 #include "config_loader.hpp"
 
-int main(void)
+int main(int argc, char **argv)
 {
     int windowWidth, windowHeight;
 
+    // initialize sdl
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
         std::cerr << "Failed to initialize SDL: " << SDL_GetError() << std::endl;
 	return 1;
     }
 
+    // make the window
     SDL_Window *window = SDL_CreateWindow("super GAMING menu", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 3, 3, SDL_WINDOW_BORDERLESS);
     if (!window) {
         std::cerr << "Failed to create window: " << SDL_GetError() << std::endl;
 	return 1;
     }
 
+    // make the renderer
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     if (!renderer) {
         std::cerr << "Failed to create renderer: " << SDL_GetError() << std::endl;
@@ -27,8 +30,10 @@ int main(void)
 	return 1;
     }
 
+    // enable transparency
     SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
 
+    // intiialize text stuff
     if (TTF_Init() != 0) {
         std::cerr << "Failed to initialize SDL_ttf: " << SDL_GetError() << std::endl;
         SDL_DestroyRenderer(renderer);
@@ -37,6 +42,8 @@ int main(void)
         return 1;
     }
 
+    // open liberation sans
+    // TODO get font from config / use default font if not in config
     TTF_Font *font = TTF_OpenFont("/usr/share/fonts/liberation-sans/LiberationSans-Regular.ttf", 24);
     if (!font) {
         std::cerr << "Failed to open font: " << TTF_GetError() << std::endl;
@@ -47,6 +54,7 @@ int main(void)
         return 1;
     }
 
+    // initialize image support for every image type imaginable
     int imgInitFlags = IMG_INIT_PNG | IMG_INIT_JPG | IMG_INIT_TIF | IMG_INIT_WEBP | IMG_INIT_AVIF | IMG_INIT_JXL;
     if (!(IMG_Init(imgInitFlags) & imgInitFlags)) {
         std::cerr << "Failed to initialize SDL_image: " << IMG_GetError() << std::endl;
@@ -57,7 +65,17 @@ int main(void)
         SDL_Quit();
     }
 
-    Menu menu = loadConfig("config.json", font, renderer, &windowWidth, &windowHeight);
+    std::string configFileName;
+
+    // if there's an argument then try using that
+    if (argc >= 2) {
+        configFileName = argv[1];
+    } else {
+        // TODO cross-platform
+        configFileName = "~/.config/super-GAMING-menu/config.json";
+    }
+
+    Menu menu = loadConfig(configFileName, font, renderer, &windowWidth, &windowHeight);
 
     SDL_SetWindowSize(window, windowWidth, windowHeight);
 
